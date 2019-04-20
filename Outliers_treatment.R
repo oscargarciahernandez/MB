0439d18786c70a3402627fbdae14db899438b0e8
oscargarciahernandez <garcia96.oscar@gmail.com>
2019-04-20 19:46:39 +0200

    Avanzando en la comprobacion de los datos de aportacion frente a nivel

--- Outliers_treatment.R
+++ Outliers_treatment.R
@@ -100,24 +100,35 @@ ccf_belesar$lag[which.max(ccf_belesar$acf)]
 ##Pasamos a tratar la Lluvia
 #ojito con la utilidad del comando lag y el comando lead
 
-DHI$`LLUVIA ACUMULADA DÍA (l/m2)`<- lead(DHI$`LLUVIA ACUMULADA DÍA (l/m2)`)
+DHI$`LLUVIA ACUMULADA DÍA (l/m2)`<- lead(DHI$`LLUVIA ACUMULADA DÃA (l/m2)`)
 
-Prueba_desacumular<- DHI %>% group_by(yday(DATE), year(DATE)) %>% summarize(oye= sum(diff(`LLUVIA ACUMULADA DÍA (l/m2)`)))
+Prueba_desacumular<- DHI %>% group_by(yday(DATE), year(DATE)) %>% mutate(desacumulada= c(0,diff(`LLUVIA ACUMULADA DÍA (l/m2)`)),
+                                                                         lluvia=na.interpolation(ifelse(desacumulada>=0, desacumulada, NA)))
 
 
+
+#outlierKD(Prueba_desacumular, lluvia)
 
 View(DHI)
 acum_diaria<- seq(range(DHI$DATE)[1],range(DHI$DATE)[2],
                   by="hour")
-prueba<- DHI %>% group_by(hour(DATE), yday(DATE), year(DATE))  %>% summarize(., sum(desacumulada, na.rm = T))
+
+Prueba_desacumular$`yday(DATE)`<- NULL
+Prueba_desacumular$`year(DATE)`<- NULL
+
+prueba<- as.data.frame(Prueba_desacumular) %>% group_by(hour(DATE), yday(DATE), year(DATE))  %>% 
+  summarize(., Acum_horaria=sum(lluvia, na.rm = T),
+            aport_mean=mean(`APORTACION (m3/s)`, na.rm = T),
+            nivel_mean=mean(`NIVEL EMBALSE (msnm)`, na.rm = T))
 
 
 Lluvia_acum_horaria<- as.data.frame(cbind(as.character(acum_diaria[2:length(acum_diaria)]), 
-                                          prueba$`sum(desacumulada, na.rm = T)`))
-Lluvia_acum_horaria$V1<- ymd_hms(Lluvia_acum_horaria$V1)
+                                          prueba[,4:6]))
+
+Lluvia_acum_horaria$`as.character(acum_diaria[2:length(acum_diaria)])`<- ymd_hms(Lluvia_acum_horaria$`as.character(acum_diaria[2:length(acum_diaria)])`)
 
 
-colnames(Lluvia_acum_horaria)<- c("Date", "Lluvia_mm")
+colnames(Lluvia_acum_horaria)<- c("Date", "Lluvia_mm", "aport_mean", "nivel_mean")
 
 
 #Añadimos la aportación y el resto de variables
@@ -128,15 +139,95 @@ nivel_SMA<- SMA(DHI$`NIVEL EMBALSE (msnm)`, x)
 aportacion_horaria<- aportacion_SMA[DHI$DATE%in%acum_diaria]
 nivel_horario<- nivel_SMA[DHI$DATE%in%acum_diaria]
 
+y<- 24*5
+aportacion_mean_SMA<- SMA(prueba$aport_mean,y)
+nivel_mean_SMA<- SMA(prueba$nivel_mean,y)
+plot(aportacion_mean_SMA)
+
+
 
 Tabla_DHI<- as.data.frame(cbind(Lluvia_acum_horaria, 
                                 aportacion_horaria[2:length(aportacion_horaria)],
-                                nivel_horario[2:length(nivel_horario)]))
+                                nivel_horario[2:length(nivel_horario)], 
+                                aportacion_mean_SMA,
+                                nivel_mean_SMA))
 
 
 
-colnames(Tabla_DHI)<- c("Date", "Lluvia_mm", "Aportacion_m3_s", "Nivel_msnm")
+colnames(Tabla_DHI)<- c(names(Lluvia_acum_horaria), "aport_SMA", "nivel_SMA", "aport_mean_SMA",  "nivel_mean_SMA")
+Tabla_DHI<- Tabla_DHI[complete.cases(Tabla_DHI),]
+
+rango<- 3000:5000
+plot(Tabla_DHI$aport_mean[rango], type = "l")
+lines(Tabla_DHI$aport_SMA[rango], col= "red")
+
+plot(Tabla_DHI$nivel_mean[rango], type = "l")
+lines(Tabla_DHI$nivel_SMA[rango], col= "red")
 
 
-View(Tabla_DHI)
+plot(Tabla_DHI$aport_mean_SMA[rango], type = "l", ylim= c(50,500))
+lines(Tabla_DHI$nivel_mean_SMA[rango], col= "red")
+
+plot(Tabla_DHI$aport_mean[rango], type = "l", ylim= c(50,500))
+lines(Tabla_DHI$nivel_mean[rango], col= "red")
+
+plot(Tabla_DHI$aport_SMA[rango], type = "l", ylim= c(50,500))
+lines(Tabla_DHI$nivel_SMA[rango], col= "red")
+
+
+cor(Tabla_DHI$aport_mean_SMA[rango],
+    Tabla_DHI$nivel_mean_SMA[rango])
+
+
+
+ccf_aport_mean<- ccf(Tabla_DHI$aport_mean_SMA[rango],
+    Tabla_DHI$nivel_mean_SMA[rango], lag.max = 1000)
+
+ccf_aport_mean$lag[which.max(ccf_aport_mean$acf)]
+
+
+cor(Tabla_DHI$aport_SMA[rango],
+    Tabla_DHI$nivel_SMA[rango])
+
+
+
+ccf_aport_mean<- ccf(Tabla_DHI$aport_SMA,
+                     Tabla_DHI$nivel_SMA, lag.max = 10000)
+
+max(ccf_aport_mean$acf)
+ccf_aport_mean$lag[which.max(ccf_aport_mean$acf)]
+
+
+# Compromar cual es la mejor SMA La conclusion que se saca es que para 5 dias de SMA se consigue la mejor Corr
+------------------------------------------
+
+for (n in 1:14 ) {
+  y<- 24*n
+  aportacion_mean_SMA<- SMA(prueba$aport_mean,y)
+  nivel_mean_SMA<- SMA(prueba$nivel_mean,y)
+  
+  
+  
+  Tabla_DHI<- as.data.frame(cbind(Lluvia_acum_horaria, 
+                                  aportacion_horaria[2:length(aportacion_horaria)],
+                                  nivel_horario[2:length(nivel_horario)], 
+                                  aportacion_mean_SMA,
+                                  nivel_mean_SMA))
+  
+  
+  
+  colnames(Tabla_DHI)<- c(names(Lluvia_acum_horaria), "aport_SMA", "nivel_SMA", "aport_mean_SMA",  "nivel_mean_SMA")
+  Tabla_DHI<- Tabla_DHI[complete.cases(Tabla_DHI),]
+  
+
+  ccf_aport_mean<- ccf(Tabla_DHI$aport_mean_SMA[rango],
+                       Tabla_DHI$nivel_mean_SMA[rango], lag.max = 1000)
+  
+  print(paste0("Máxima correlacion de ", 
+               round(max(ccf_aport_mean$acf), digits = 3),
+               " .Para un desfase de: ",
+        round(ccf_aport_mean$lag[which.max(ccf_aport_mean$acf)]/24, digits = 2),
+        " días. Con n= ", n))
+ 
+}
 
