5433a3a0533f2fae5fbd27e1faa7c849810fc28f
oscargarciahernandez <garcia96.oscar@gmail.com>
2019-07-01 11:30:20 +0200

    COSITAS VARIAS

--- READ_GRIBS.R
+++ READ_GRIBS.R
@@ -1,5 +1,5 @@
 library(rNOMADS)
-library(gdal)
+library(rgdal)
 library(here)
 source('libraries.R')
 
@@ -7,7 +7,7 @@ source('libraries.R')
 "https://www.nco.ncep.noaa.gov/pmb/products/gfs/gfs.t00z.pgrb2.0p25.f006.shtml" %>% GET() %>% 
   htmlParse() %>% readHTMLTable()
 
-info_grib<- GribInfo(here::here('Gribs/20190624/19062400.gfs.t00z.0p25.pgrb2f000'))
+info_grib<- GribInfo(here::here('Gribs/'))
 all_variables<- info_grib$inventory %>% str_split(":") %>% sapply(function(x) x[4]) %>% unique()
 all_levels<- info_grib$inventory %>% str_split(":") %>% sapply(function(x) x[5]) %>% unique()
 
